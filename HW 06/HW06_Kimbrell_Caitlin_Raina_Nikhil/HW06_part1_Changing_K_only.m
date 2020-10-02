1d6d9c3a7232bcbb23dacbb89283cb20580c8859
Caitlin Kimbrell <cbk3451@g.rit.edu>
2020-10-02 17:34:35 -0400

    added comments

--- HW 06/HW06_Kimbrell_Caitlin_Raina_Nikhil/HW06_part1_Changing_K_only.m
+++ HW 06/HW06_Kimbrell_Caitlin_Raina_Nikhil/HW06_part1_Changing_K_only.m
@@ -1,8 +1,12 @@
+% load the pictures and convert to doubles
 im_macbeth = im2double(imread('HW_06_MacBeth_Regular.jpg'));
 im_balls = im2double(imread('BALLS_FIVE_7537_shrunk.jpg'));
+
+% convert to indexes and recieve the image and a colormap
 [im_macbeth_ind, macbeth_map] = rgb2ind( im_macbeth , 27, 'nodither');
 [im_balls_ind, ball_map] = rgb2ind(im_balls, 15, 'nodither');
 
+% display new images and colormaps
 imagesc(im_macbeth_ind);
 colormap(macbeth_map);
 figure
