cc35ac11c63ff1f29b419c27f24741c9781ca0a0
Caitlin Kimbrell <cbk3451@g.rit.edu>
2020-10-10 23:39:35 -0400

    added comments

--- Project/file_reader.m
+++ Project/file_reader.m
@@ -1,5 +1,7 @@
 function file_reader()
-    for idx = 107:159
+% currently our function works well for scans 120 - 132
+% not as effective with the other iamge scans and we are working on a fix
+    for idx = 120:132
         file_name = strcat('PROJ_IMAGES/SCAN0',int2str(idx),'.jpg');
         if exist(file_name, 'file') ~= 0
             project(file_name);
