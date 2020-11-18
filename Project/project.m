13f2a7541fa062c1450c1fe82df52ff75de8b109
Nikhil Raina <nxr5013@g.rit.edu>
2020-11-17 23:53:37 -0500

    Displays the required image completely

--- Project/project.m
+++ Project/project.m
@@ -1,17 +1,16 @@
 function project(file_name)
-    im = imread(file_name);
-    %im = imread('PROJ_IMAGES/SCAN0125.jpg');
+    %im = imread(file_name);
+    im = imread('PROJ_IMAGES/SCAN0124.jpg');
     BOX_LIMIT = 60;
     
     % 1: word list
     % 2: boxes list
     % 3: cropped puzzle image in gray
     % 4: empty boxes where the de-jumbled word shall go in.
-    [word_list, box_list, im_puzzle, empty_boxes] = preprocess(im);
-    dejumble_words = dejumble(word_list);
-    %dejumble_words = ["gerbil"; "??????"; "digest"; "afloat"; "hungry"; "unload"];
-    %Iname = insertObjectAnnotation(im_puzzle,'rectangle', box_list, dejumble_words);
-    %imshow(Iname);
+    % 5: centers of the circles
+    % 6: the radii of the circles
+    [word_list, box_list, im_puzzle, empty_boxes, centers, radii] = preprocess(im);
+    %dejumble_words = ["gerbil"; "driver"; "digest"; "afloat"; "hungry"; "unload"];
     
     letter_sequence = string(cell(0,0));
     coordinates = [];
@@ -31,7 +30,16 @@ function project(file_name)
     
     de_jumbled_display = insertText(im_puzzle, coordinates, letter_sequence, ...
                         'FontSize', 36, 'TextColor', 'blue', 'BoxOpacity', 0.0);
-    %set( gcf(), 'Position',  [ 155, -300, 1400, 900 ] );
     imshow(de_jumbled_display);
+    hold on;
+    
+    % makes the circles visible
+    viscircles(centers, radii, 'Color', 'b', 'LineWidth', 4);
+    
+    % prints our all the Squares
+    for index = 1:length(empty_boxes)
+        rectangle('position',empty_boxes(index, :),'edgecolor','r','linewidth',4);
+    end
+    
     title('\fontsize{20}De-Jumbled Puzzle');
 end
