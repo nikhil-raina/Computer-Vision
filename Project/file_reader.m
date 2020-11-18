ab45f91158a46a5b4115b588a8cd8da70542e8fb
Nikhil Raina <nxr5013@g.rit.edu>
2020-11-17 20:54:53 -0500

    added a decent limit to the pause for the file reader

--- Project/file_reader.m
+++ Project/file_reader.m
@@ -1,11 +1,12 @@
 function file_reader()
 % currently our function works well for scans 120 - 132
 % not as effective with the other iamge scans and we are working on a fix
-    for idx = 120:132
+    for idx = 125:132
         file_name = strcat('PROJ_IMAGES/SCAN0',int2str(idx),'.jpg');
         if exist(file_name, 'file') ~= 0
             project(file_name);
-            pause(1)
+            pause(8);
+            close all;
         end
     end
 end
